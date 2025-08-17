f9a258c67cbf0a031534efe3c51794a97a4c20b8
Giorgi <okropiridze.geo@gmail.com>
2025-08-17 17:12:18 +0200

    setup.py choice 22

--- gdpr/migrations/0002_auto_20180509_1518.py
+++ gdpr/migrations/0002_auto_20180509_1518.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 import datetime
 
 from django.db import migrations, models
-from django.utils.timezone import utc
+from datetime import timezone
 
 
 class Migration(migrations.Migration):
